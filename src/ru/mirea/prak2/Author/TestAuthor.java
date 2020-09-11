6baf1f9475e4785ab6e7e23a00f4956c26d9a195
4kul4 <maletskyamargarita@gmail.com>
2020-09-11 17:43:03 +0300

    Initial commit

---
+++ src/ru/mirea/prak2/Author/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.prak2.Author;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author a1 = new Author("Bob", "bob@gmail.com", 'M');
+        System.out.println(a1);
+    }
+
+}
